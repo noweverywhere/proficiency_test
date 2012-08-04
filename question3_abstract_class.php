e25f2330150fd0e72f410082c40a704104764f1c
Marinus Swanepoel <marinus@noweverywhere.ca>
2012-08-03 23:30:45 -0400

    answered question 3 about abstract classes

---
+++ question3_abstract_class.php
@@ -0,0 +1,31 @@
+<?php 
+
+/**
+* Question 3:
+* Using the language of your choice, demonstrate the use of and abstract class.
+*/
+
+abstract class Q3AbstraceClass
+{
+	public function say_hello($name = 'world') 
+	{
+		return "Hello $name!";
+	}
+}
+// one can not instantiate the Abstract class:
+// $helloWorld = new Q3AbstraceClass;
+
+
+// but it is possible to extend it
+class Greetings extends Q3AbstraceClass
+{
+	public function in_french($name = 'world')
+	{
+		return "Bonjour $name!";
+	}
+}
+
+// and instantiate the class that extends the abstact class
+$helloWorld = new Greetings;
+// and then use methods that are defined by the abstract class
+echo $HelloWorld->say_hello();
